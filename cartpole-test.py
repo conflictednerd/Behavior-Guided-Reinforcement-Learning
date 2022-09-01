8c5f98c457316cdc495af974f78148cccfd8483c
hedayatians <hedayatians@gmail.com>
2022-09-01 19:58:26 +0430

    Organized hyperparameters

--- cartpole-test.py
+++ cartpole-test.py
@@ -8,6 +8,7 @@ import optax
 from numpyro.distributions.discrete import Categorical
 
 from data.storage import DictList
+from hyperparams import get_args
 
 """
 This file serve as a test bed for sanity checking my implementations with the cartpole environment.
@@ -24,21 +25,7 @@ TODO: Add a critic network and use it to add a baseline to the RL objective
 ? Right now I'm getting a score of ~285 after 20 epochs of training.
 """
 
-#! Attention: num_envs * rollout_len must be divisable by batch_size for jit to work correctly
-args = {
-    'epochs': 20,
-    'num_envs': 4,
-    'rollout_len': 1000,
-    'batch_size': 500,
-    'run_name': 'CartPoleTest',
-    'env_id': 'CartPole-v1',
-    'seed': 23,
-    'capture_video': False,
-    'gamma': 0.99,  # discount factor
-    # Number of times a collected (full) batch of experiences is used to update the networks. -> If it is too large, the importance ratios may get inaccurate.
-    'iters_per_epoch': 2,
-}
-
+args = get_args()
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
@@ -58,20 +45,20 @@ def make_env(env_id, seed, idx, capture_video, run_name):
 def train(rng):
     # ATTENTION: SyncVectorEnv automatically resets the envs when they are done
     envs = gym.vector.SyncVectorEnv([make_env(
-        args['env_id'], args['seed'] + i, i, args['capture_video'], args['run_name']) for i in range(args['num_envs'])])
+        args.env_id, args.seed + i, i, args.capture_video, args.exp_name) for i in range(args.num_envs)])
 
+    rng, actor_rng, critic_rng = random.split(rng, 3)
     actor = hk.transform(lambda x: hk.nets.MLP(
         [64, 2], activation=jnp.tanh)(x))
-    rng, actor_rng = random.split(rng)
-    actor_params = actor.init(rng=actor_rng, x=jnp.zeros(4))
+    actor_params = actor.init(rng=actor_rng, x=jnp.zeros(envs.single_observation_space.shape))
 
     optimizer = optax.chain(
         optax.clip(1.0),
-        optax.adam(learning_rate=1e-2),
+        optax.adam(learning_rate=args.lr),
     )
     optimizer_state = optimizer.init(actor_params)
 
-    for epoch in range(args['epochs']):
+    for epoch in range(args.epochs):
         rng, buffer_rng, learn_rng = random.split(rng, 3)
         print('Collecting samples...')
         buffer = fill_buffer(envs, actor_params, actor, buffer_rng)
@@ -79,7 +66,7 @@ def train(rng):
         actor_params, optimizer_state, stats = learn(
             buffer, actor, actor_params, optimizer, optimizer_state, learn_rng)
         print(
-            f'epoch:\t{epoch}\t loss:\t{stats["avg_loss"]}\t score:\t{stats["avg_reward"]}')
+            f'epoch:\t{epoch+1}\t loss:\t{stats["avg_loss"]}\t score:\t{stats["avg_reward"]}')
         # evaluate()
         del buffer
 
@@ -89,7 +76,7 @@ def fill_buffer(envs, actor_params, actor, rng):
     run the vectorized env for n steps and put the experiences in the buffer
     return the flattened buffer
     '''
-    buffer = DictList((args['rollout_len'], args['num_envs']), info={
+    buffer = DictList((args.num_steps, args.num_envs), info={
         'obs': envs.single_observation_space.shape,
         'act': envs.single_action_space.shape,
         'rew': 1,
@@ -97,35 +84,35 @@ def fill_buffer(envs, actor_params, actor, rng):
         'done': 1,
         'returns': 1,
         'adv': 1,
-        # This is the log probability of the selected action. Used to compute importance weights for off-policy updates.
+        # log probability of the selected action. Used to compute importance weights for off-policy updates.
         'logp': 1,
     })
-    next_obs = envs.reset()
-    next_done = np.zeros((args['num_envs'],))
 
-    for i in range(args['rollout_len']):
+    next_obs = envs.reset()
+    next_done = np.zeros((args.num_envs,))
+    for t in range(args.num_steps):
         rng, actor_rng, sample_rng = random.split(rng, 3)
         logits = actor.apply(params=actor_params, x=next_obs, rng=actor_rng)
         # Todo: modify actor networks to return a distribution object instead of logits
         actions = np.array(Categorical(logits=logits).sample(sample_rng))
-        buffer[i] = {'obs': next_obs, 'act': actions, 'done': next_done,
+        buffer[t] = {'obs': next_obs, 'act': actions, 'done': next_done,
                      'logp': Categorical(logits=logits).log_prob(actions)}
         next_obs, reward, done, info = envs.step(actions)
-        buffer[i] = {'rew': reward, 'next_obs': next_obs}
+        buffer[t] = {'rew': reward, 'next_obs': next_obs}
 
         next_obs, next_done = np.array(next_obs), np.array(done)
 
     # compute reward2gos
     assert len(buffer.shape) == 2
-    for t in reversed(range(args['rollout_len'])):
-        if t == args['rollout_len']-1:
+    for t in reversed(range(args.num_steps)):
+        if t == args.num_steps-1:
             next_nonterminal = 1-next_done
-            next_return = np.zeros(args['num_envs'])  # critic(next_obs)
+            next_return = np.zeros(args.num_envs)  # critic(next_obs)
         else:
             next_nonterminal = 1 - buffer[t+1]['done']
             next_return = buffer[t+1]['returns']
         buffer[t] = {'returns': buffer[t]['rew'] +
-                     args['gamma']*next_nonterminal*next_return}
+                     args.gamma*next_nonterminal*next_return}
 
     # adv = returns - values
     # ! Caution: buffer[:]['adv'] will not update in place
@@ -176,11 +163,11 @@ def learn(buffer, actor, actor_params, optimizer, optimizer_state, rng):
     # * compute the loss
     # * update the networks
     total_loss = 0  # todo report average reward as well
-    for i in range(args['iters_per_epoch']):
+    for i in range(args.iters_per_epoch):
         rng, shuffle_rng = random.split(rng)
         indices = random.permutation(shuffle_rng, len(buffer))
-        for j in range(0, len(buffer), args['batch_size']):
-            mini_batch = buffer[indices[j: j + args['batch_size']]]
+        for j in range(0, len(buffer), args.mini_batch_size):
+            mini_batch = buffer[indices[j: j + args.mini_batch_size]]
             rng, mb_rng = random.split(rng)
             loss, grads = jax.value_and_grad(policy_loss)(
                 actor_params, actor, mini_batch, mb_rng)
@@ -191,8 +178,8 @@ def learn(buffer, actor, actor_params, optimizer, optimizer_state, rng):
 
             total_loss += loss
 
-    avg_loss = total_loss / args['iters_per_epoch']
-    avg_reward = jnp.sum(buffer['rew']) / (jnp.sum(buffer['done']) + args['num_envs'] -
+    avg_loss = total_loss / args.iters_per_epoch
+    avg_reward = jnp.sum(buffer['rew']) / (jnp.sum(buffer['done']) + args.num_envs-
                                            jnp.sum(buffer[-1]['done']))  # divided by the number of episodes in the buffer
 
     stats = {
@@ -208,4 +195,4 @@ def evaluate():
 
 
 if __name__ == '__main__':
-    train(random.PRNGKey(23))
+    train(random.PRNGKey(args.seed))
---
+++ hyperparams.py
@@ -0,0 +1,35 @@
+import argparse
+from distutils.util import strtobool
+
+
+def get_args(arg_set: str = 'default') -> argparse.Namespace:
+    if arg_set == 'default':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--exp-name', type=str, default='test-name')
+        parser.add_argument('--seed', type=int, default=23)
+        parser.add_argument('--capture-video', type=lambda x: bool(strtobool(x)), default=False, nargs='?', const=True,
+                            help="whether to capture videos of the agent (saved in the `videos` directory)")
+
+        parser.add_argument('--env-id', type=str, default='CartPole-v1',
+                            help='the id of the env (must by gym-registered)')
+        parser.add_argument('--gamma', type=float, default=0.99,
+                            help='discount factor')
+        parser.add_argument('--num-envs', type=int, default=4,
+                            help='number of parallel envs used for data collection')
+        parser.add_argument('--num-steps', type=int, default=1000,
+                            help='number of steps to run each env for a rollout')
+        parser.add_argument('--mini-batch-size', type=int, default=500,
+                            help='number of transition samples in each mini-batch')
+        parser.add_argument('--epochs', type=int, default=20,
+                            help='number of training epochs')
+        parser.add_argument('--iters-per-epoch', type=int, default=2,
+                            help='number of times data buffer is iterated to update the networks')
+        parser.add_argument('--lr', type=float, default=1e-3,
+                            help="optimizer's learning rate")
+
+        args = parser.parse_args()
+        # ! Sanity checks:
+        assert (args.num_envs * args.num_steps) % args.mini_batch_size == 0
+        return args
+    else:
+        raise NotImplementedError
