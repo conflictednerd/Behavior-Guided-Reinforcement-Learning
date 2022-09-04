beafdedbc9d975dd5abcfcf83b2c2b4a37b30300
hedayatians <hedayatians@gmail.com>
2022-09-04 05:35:32 +0430

    Changed some hyperparameters

--- hyperparams.py
+++ hyperparams.py
@@ -14,17 +14,17 @@ def get_args(arg_set: str = 'default') -> argparse.Namespace:
                             help='the id of the env (must by gym-registered)')
         parser.add_argument('--gamma', type=float, default=0.99,
                             help='discount factor')
-        parser.add_argument('--num-envs', type=int, default=8,
+        parser.add_argument('--num-envs', type=int, default=4,
                             help='number of parallel envs used for data collection')
-        parser.add_argument('--num-steps', type=int, default=512,
+        parser.add_argument('--num-steps', type=int, default=1024,
                             help='number of steps to run each env for a rollout')
         parser.add_argument('--mini-batch-size', type=int, default=256,
                             help='number of transition samples in each mini-batch')
-        parser.add_argument('--epochs', type=int, default=20,
+        parser.add_argument('--epochs', type=int, default=10,
                             help='number of training epochs')
-        parser.add_argument('--iters-per-epoch', type=int, default=2,
+        parser.add_argument('--iters-per-epoch', type=int, default=4,
                             help='number of times data buffer is iterated to update the networks')
-        parser.add_argument('--lr', type=float, default=1e-2,
+        parser.add_argument('--lr', type=float, default=1e-3,
                             help="optimizer's learning rate")
 
         args = parser.parse_args()
