df3767af6b6a3d6222c3779866270146b003a99f
hedayatians <hedayatians@gmail.com>
2022-09-11 17:16:40 +0430

    Added MiniGrid integration

--- hyperparams.py
+++ hyperparams.py
@@ -3,47 +3,57 @@ from distutils.util import strtobool
 
 
 def get_args(arg_set: str = 'default') -> argparse.Namespace:
-    if arg_set == 'default':
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--exp-name', type=str, default='test-name')
-        parser.add_argument('--seed', type=int, default=23)
-        parser.add_argument('--capture-video', type=lambda x: bool(strtobool(x)), default=False, nargs='?', const=True,
-                            help="whether to capture videos of the agent (saved in the `videos` directory)")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp-name', type=str, default='test-name')
+    parser.add_argument('--seed', type=int, default=23)
+    parser.add_argument('--capture-video', type=lambda x: bool(strtobool(x)), default=False, nargs='?', const=True,
+                        help="whether to capture videos of the agent (saved in the `videos` directory)")
 
-        parser.add_argument('--env-id', type=str, default='CartPole-v1',
-                            help='the id of the env (must by gym-registered)')
-        parser.add_argument('--gamma', type=float, default=0.99,
-                            help='discount factor')
-        parser.add_argument('--gae_lambda', type=float,
-                            default=0.95, help='lambda used for computing GAE')
-        parser.add_argument('--num-envs', type=int, default=4,
-                            help='number of parallel envs used for data collection')
-        parser.add_argument('--num-steps', type=int, default=1000,
-                            help='number of steps to run each env for a rollout')
-        parser.add_argument('--mini-batch-size', type=int, default=250,
-                            help='number of transition samples in each mini-batch')
-        parser.add_argument('--epochs', type=int, default=20,
-                            help='number of training epochs')
-        parser.add_argument('--iters-per-epoch', type=int, default=4,
-                            help='number of times data buffer is iterated to update the networks')
-        parser.add_argument('--lr', type=float, default=1e-3,
-                            help="optimizer's learning rate")
+    parser.add_argument('--env-id', type=str, default='CartPole-v1',
+                        help='the id of the env (must by gym-registered)')
+    parser.add_argument('--gamma', type=float, default=0.99,
+                        help='discount factor')
+    parser.add_argument('--gae_lambda', type=float,
+                        default=0.95, help='lambda used for computing GAE')
+    parser.add_argument('--num-envs', type=int, default=4,
+                        help='number of parallel envs used for data collection')
+    parser.add_argument('--num-steps', type=int, default=1000,
+                        help='number of steps to run each env for a rollout')
+    parser.add_argument('--mini-batch-size', type=int, default=250,
+                        help='number of transition samples in each mini-batch')
+    parser.add_argument('--epochs', type=int, default=20,
+                        help='number of training epochs')
+    parser.add_argument('--iters-per-epoch', type=int, default=4,
+                        help='number of times data buffer is iterated to update the networks')
+    parser.add_argument('--lr', type=float, default=1e-3,
+                        help="optimizer's learning rate")
 
-        # actor-critic arguments
-        parser.add_argument('--clip-coef', type=float, default=0.2,
-                            help="ppo's surrogate loss coefficient")
-        parser.add_argument('--vf-coef', type=float,
-                            default=0.1, help="coefficient of value loss")
-        parser.add_argument('--ent-coef', type=float,
-                            default=0.05, help="ppo's entropy loss coefficient")
-        parser.add_argument('--clip-vloss', type=lambda x: bool(strtobool(x)), default=True,
-                            nargs="?", const=True, help="controls whether ppo uses a clipped value loss or not")
-        parser.add_argument('--target-kl', type=float,
-                            default=None, help="target KL-divergence threshold")
+    # actor-critic arguments
+    parser.add_argument('--clip-coef', type=float, default=0.2,
+                        help="ppo's surrogate loss coefficient")
+    parser.add_argument('--vf-coef', type=float,
+                        default=0.1, help="coefficient of value loss")
+    parser.add_argument('--ent-coef', type=float,
+                        default=0.05, help="ppo's entropy loss coefficient")
+    parser.add_argument('--clip-vloss', type=lambda x: bool(strtobool(x)), default=True,
+                        nargs="?", const=True, help="controls whether ppo uses a clipped value loss or not")
+    parser.add_argument('--target-kl', type=float,
+                        default=None, help="target KL-divergence threshold")
 
-        args = parser.parse_args()
-        # ! Sanity checks:
-        assert (args.num_envs * args.num_steps) % args.mini_batch_size == 0
-        return args
-    else:
-        raise NotImplementedError
+    args = parser.parse_args()
+
+    if arg_set == 'minigrid':
+        args.capture_video = True
+        args.env_id = 'MiniGrid-Empty-5x5-v0'
+        args.gamma = 0.9
+        args.ent_coef = 0.01
+        args.vf_coef = 0.5
+        args.num_steps = 1000
+        args.mini_batch_size = 250
+        args.lr = 1e-2
+        args.epochs = 20
+
+    # ! Sanity checks:
+    assert (args.num_envs * args.num_steps) % args.mini_batch_size == 0
+
+    return args
---
+++ minigrid-test.py
@@ -0,0 +1,178 @@
+import functools
+import operator
+
+import gym
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import jax.random as random
+import numpy as np
+import optax
+from gym.core import ObservationWrapper
+from gym_minigrid.wrappers import FullyObsWrapper, ImgObsWrapper
+
+import data.collector as collector
+from hyperparams import get_args
+from networks.actor import MLPActor
+from networks.common import MLP
+from RL.fast import ppo_loss_and_grad
+
+args = get_args('minigrid')
+
+
+class RewardWrapper(gym.Wrapper):
+    """
+    Wrapper which adds an exploration bonus.
+    This is a reward to encourage exploration of less
+    visited (state,action) pairs.
+    """
+
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        reward -= 0.01
+
+        return obs, reward, terminated, truncated, info
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+
+class FlatImgDirObsWrapper(ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+        imgSpace = env.observation_space.spaces["image"]
+        imgSize = functools.reduce(operator.mul, imgSpace.shape, 1)
+
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(imgSize+4,),
+            dtype="uint8",
+        )
+
+    def observation(self, obs):
+        direction = np.zeros(4, dtype='uint8')
+        direction[obs['direction']] = 1
+        img = obs['image'].flatten()
+        return np.append(img, direction)
+
+
+def make_env(env_id, seed, idx, capture_video, run_name):
+    # max_steps for minigrid envs is 4 x size x size
+    def thunk():
+        env = gym.make(env_id, render_mode='rgb_array')
+        env = FullyObsWrapper(env)
+        env = FlatImgDirObsWrapper(env)
+        # env = RewardWrapper(env)
+        # env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(
+                    env, f"videos/{run_name}", episode_trigger=lambda e: e % 25 == 1)  # Record every 25 episodes
+        env.reset(seed=seed+idx)
+        return env
+
+    return thunk
+
+
+def train(rng):
+    envs = gym.vector.SyncVectorEnv([make_env(
+        args.env_id, args.seed + i, i, args.capture_video, args.exp_name) for i in range(args.num_envs)])
+
+    rng, actor_rng, critic_rng = random.split(rng, 3)
+    actor = hk.transform(lambda x: MLPActor(
+        hidden_dims=[64, 64], num_actions=envs.single_action_space.n)(x))
+    actor_params = actor.init(rng=actor_rng, x=jnp.zeros(
+        envs.single_observation_space.shape))
+    actor = jax.jit(actor.apply)
+
+    critic = hk.transform(lambda x: MLP(
+        hidden_dims=[64, 64], out_dim=1)(x))
+    critic_params = critic.init(rng=critic_rng, x=jnp.zeros(
+        envs.single_observation_space.shape))
+    critic = jax.jit(critic.apply)
+
+    total_steps = args.epochs * args.iters_per_epoch * \
+        (args.num_envs * args.num_steps / args.mini_batch_size)
+    # lr_schedule = optax.cosine_decay_schedule(
+    #     args.lr, decay_steps=total_steps//2)
+    lr_schedule = optax.constant_schedule(args.lr)
+    optimizer = optax.chain(
+        # optax.clip(1.0),
+        optax.adam(learning_rate=lr_schedule),
+    )
+    optimizer_state = optimizer.init((actor_params, critic_params))
+
+    for epoch in range(args.epochs):
+        rng, buffer_rng, gae_rng, learn_rng = random.split(rng, 4)
+
+        print('Collecting samples...')
+        buffer = collector.collect_rollouts(
+            envs, (actor_params, actor), args, buffer_rng)
+        ret, adv, vals = collector.compute_gae(
+            buffer, (critic_params, critic), gae_rng, args.gamma, args.gae_lambda)
+        buffer['returns'][:] = ret
+        buffer['adv'][:] = adv
+        buffer['value'][:] = vals
+
+        buffer.flatten()
+
+        print('Optimizing...')
+
+        actor_params, critic_params, optimizer_state, stats = ppo_learn(
+            buffer, (actor, actor_params), (critic, critic_params), (optimizer, optimizer_state), args, learn_rng)
+        print(
+            f'epoch:\t{epoch+1}\t loss:\t{stats["avg_loss"]:.2f}\t score:\t{stats["avg_reward"]:.2f}\t a_loss:\t{stats["policy_loss"]:.2f}\t c_loss:\t{stats["value_loss"]:.2f}\t e_loss:\t{stats["entropy_loss"]:.2f}'
+        )
+        del buffer
+
+
+def ppo_learn(buffer, actor, critic, optimizer, args, rng):
+    actor, actor_params = actor
+    critic, critic_params = critic
+    optimizer, optimizer_state = optimizer
+
+    total_loss = 0
+    for i in range(args.iters_per_epoch):
+        rng, shuffle_rng = random.split(rng)
+        indices = random.permutation(shuffle_rng, len(buffer))
+        for j in range(0, len(buffer), args.mini_batch_size):
+            mini_batch = buffer[indices[j: j+args.mini_batch_size]]
+            rng, loss_rng = random.split(rng)
+            ppo_args = {
+                'clip_coef': args.clip_coef,
+                'ent_coef': args.ent_coef,
+                'vf_coef': args.vf_coef,
+                'clip_vloss': args.clip_vloss,
+            }
+            (loss, stats), grads = ppo_loss_and_grad(
+                (actor_params, critic_params), actor, critic, mini_batch, loss_rng, **ppo_args)
+            updates, optimizer_state = optimizer.update(
+                grads, optimizer_state, (actor_params, critic_params))
+            actor_params, critic_params = optax.apply_updates(
+                (actor_params, critic_params), updates)
+            total_loss += loss
+
+        if args.target_kl is not None and stats['approx_kl'] > args.target_kl:
+            break
+
+    # TODO: explained variance
+    avg_loss = total_loss / args.iters_per_epoch
+    avg_reward = jnp.sum(buffer['rew']) / (jnp.sum(buffer['done']) + args.num_envs -
+                                           jnp.sum(buffer[-1]['done']))  # divided by the number of episodes in the buffer
+    stats['avg_loss'] = avg_loss
+    stats['avg_reward'] = avg_reward
+
+    return actor_params, critic_params, optimizer_state, stats
+
+
+if __name__ == '__main__':
+    train(random.PRNGKey(args.seed))
+
+
+# action_map = ['left', 'right', 'forward', 'pickup', 'drop', 'toggle', 'done']
+# env = gym.make('MiniGrid-Empty-8x8-v0', render_mode='human')
