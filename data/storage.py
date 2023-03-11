2ee674447caed3e52c0b348fa2cb91cd0a16bb86
conflictednerd <hedayatians@gmail.com>
2023-03-12 00:26:21 +0330

    Reformatted code

--- data/storage.py
+++ data/storage.py
@@ -9,10 +9,10 @@ from jax.tree_util import register_pytree_node_class
 
 @register_pytree_node_class
 class DictList(MutableMapping):
-    '''
+    """
     A simple Dict-List hybrid that can be used to store rollouts of fixed length across multiple environments
-    Can be accessed with either 
-        dictList['obs'] -> returns a np.array, or 
+    Can be accessed with either
+        dictList['obs'] -> returns a np.array, or
         dictList[::2] -> returns a dict of np.arrays
     To set its values we must specify the indices and provide it with a dict specifying whcih fields we want to update. E.g.,
         dictList[:5] = {'obs': np.zeros(5, 64)}
@@ -20,10 +20,10 @@ class DictList(MutableMapping):
         dictList['obs'] = np.zeros(64) -> Error!
     Instead you should use:
         dictList[:] = {'obs': np.zeros(64)}
-    '''
+    """
 
     def __init__(self, shape: Union[Tuple, int], info: Dict) -> None:
-        '''
+        """
         shape: Either an integer denoting the length of the arrays or a tuple (in case of running multiple envs in parallel, the shape could be (num_envs, length))
         info: a dictionary whose keys are the fields that we want to be stored and values are their respective dimension or shape
         Example:
@@ -34,8 +34,8 @@ class DictList(MutableMapping):
             'reward': 1,
             'done': 1,
         }
-        '''
-        self.shape = (shape, ) if isinstance(shape, int) else shape
+        """
+        self.shape = (shape,) if isinstance(shape, int) else shape
         self.info = info
         self.length = 1
         for d in self.shape:
@@ -46,25 +46,28 @@ class DictList(MutableMapping):
         for key, val in info.items():
             val = tuple() if val == 1 else val
             self.data[key] = np.zeros(
-                self.shape + val if isinstance(val, tuple) else (val, ))
+                self.shape + val if isinstance(val, tuple) else (val,)
+            )
 
-    def flatten(self, ) -> None:
-        '''
+    def flatten(
+        self,
+    ) -> None:
+        """
         Flattens all arrays (in case self.shape is a tuple)
         shape of arrays change from ((base_shape) + (dims)) -> (-1, (dims))
-        '''
+        """
         for key, val in self.data.items():
-            val.shape = (-1, ) + val.shape[len(self.shape):]
+            val.shape = (-1,) + val.shape[len(self.shape) :]
 
-        self.shape = (self.length, )
+        self.shape = (self.length,)
 
     def __setitem__(self, __k, __v) -> None:
-        '''
+        """
         If obs is a field in the buffer whose values are 64x64 arrays, you can do this:
         dictList[0] = {'obs': np.zeros((64, 64))}
         If you also have 1d actions, you can do this:
         dictList[:2] = {'obs': np.zeros((2, 64, 64)), 'actions': np.zeros((2,))}
-        '''
+        """
         if isinstance(__k, str):
             # Invokation of this should be avoided: Once the np.arrays are created, they should not be tweaked with except by changing the values of a given slice.
             raise NotImplementedError
@@ -85,13 +88,19 @@ class DictList(MutableMapping):
     def __iter__(self):
         raise NotImplementedError
 
-    def keys(self, ):
+    def keys(
+        self,
+    ):
         return self.data.keys()
 
-    def values(self, ):
+    def values(
+        self,
+    ):
         return self.data.values()
 
-    def items(self, ):
+    def items(
+        self,
+    ):
         return self.data.items()
 
     def __len__(self) -> int:
@@ -99,17 +108,19 @@ class DictList(MutableMapping):
 
     @staticmethod
     def extend(dl1, dl2):
-        '''
+        """
         TODO:
         Take in two DictLists with similar keys and append their data together to create a new DictList
         Useful for aggregating multiple parallel rollout workers
-        '''
+        """
         pass
 
-    def tree_flatten(self, ):
+    def tree_flatten(
+        self,
+    ):
         aux_data = {
-            'shape': self.shape,
-            'info': self.info,
+            "shape": self.shape,
+            "info": self.info,
         }
         dict_data, additional_data = [], self.additional_data
         for key in self.info.keys():
@@ -120,9 +131,9 @@ class DictList(MutableMapping):
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
-        obj = cls(aux_data['shape'], aux_data['info'])
+        obj = cls(aux_data["shape"], aux_data["info"])
         dict_data, additional_data = children
-        for (key, val) in zip(aux_data['info'].keys(), dict_data):
+        for key, val in zip(aux_data["info"].keys(), dict_data):
             obj.data[key] = val
         obj.additional_data = additional_data
         return obj
@@ -131,12 +142,12 @@ class DictList(MutableMapping):
 # ! WARNING: INCOMPLETE AND NOT TESTED
 @register_pytree_node_class
 class JAXDictList(MutableMapping):
-    '''
+    """
     Just like DictList, but with jnp arrays instead of regular np arrays
-    '''
+    """
 
     def __init__(self, shape: Union[Tuple, int], info: Dict) -> None:
-        '''
+        """
         shape: Either an integer denoting the length of the arrays or a tuple (in case of running multiple envs in parallel, the shape could be (num_envs, length))
         info: a dictionary whose keys are the fields that we want to be stored and values are their respective dimension or shape
         Example:
@@ -147,8 +158,8 @@ class JAXDictList(MutableMapping):
             'reward': 1,
             'done': 1,
         }
-        '''
-        self.shape = (shape, ) if isinstance(shape, int) else shape
+        """
+        self.shape = (shape,) if isinstance(shape, int) else shape
         self.length = 1
         self.info = info
         for d in self.shape:
@@ -158,28 +169,32 @@ class JAXDictList(MutableMapping):
         for key, val in info.items():
             val = tuple() if val == 1 else val
             self.data[key] = jnp.zeros(
-                self.shape + val if isinstance(val, tuple) else (val, ))
+                self.shape + val if isinstance(val, tuple) else (val,)
+            )
 
         self.additional_data = []  # Contain arrays only
 
-    def flatten(self, ) -> None:
-        '''
+    def flatten(
+        self,
+    ) -> None:
+        """
         Flattens all arrays (in case self.shape is a tuple)
         shape of arrays change from ((base_shape) + (dims)) -> (-1, (dims))
-        '''
+        """
         for key, val in self.data.items():
             self.data[key] = jnp.reshape(
-                self.data[key], newshape=(-1, ) + val.shape[len(self.shape):])
+                self.data[key], newshape=(-1,) + val.shape[len(self.shape) :]
+            )
 
-        self.shape = (self.length, )
+        self.shape = (self.length,)
 
     def __setitem__(self, __k, __v) -> None:
-        '''
+        """
         If obs is a field in the buffer whose values are 64x64 arrays, you can do this:
         dictList[0] = {'obs': np.zeros((64, 64))}
         If you also have 1d actions, you can do this:
         dictList[:2] = {'obs': np.zeros((2, 64, 64)), 'actions': np.zeros((2,))}
-        '''
+        """
         if isinstance(__k, str):
             # Invokation of this should be avoided: Once the np.arrays are created, they should not be tweaked with except by changing the values of a given slice.
             raise NotImplementedError
@@ -205,13 +220,19 @@ class JAXDictList(MutableMapping):
     def __iter__(self):
         raise NotImplementedError
 
-    def keys(self, ):
+    def keys(
+        self,
+    ):
         return self.data.keys()
 
-    def values(self, ):
+    def values(
+        self,
+    ):
         return self.data.values()
 
-    def items(self, ):
+    def items(
+        self,
+    ):
         return self.data.items()
 
     def __len__(self) -> int:
@@ -219,17 +240,19 @@ class JAXDictList(MutableMapping):
 
     @staticmethod
     def extend(dl1, dl2):
-        '''
+        """
         TODO:
         Take in two DictLists with similar keys and append their data together to create a new DictList
         Useful for aggregating multiple parallel rollout workers
-        '''
+        """
         pass
 
-    def tree_flatten(self, ):
+    def tree_flatten(
+        self,
+    ):
         aux_data = {
-            'shape': self.shape,
-            'info': self.info,
+            "shape": self.shape,
+            "info": self.info,
         }
         dict_data, additional_data = [], self.additional_data
         for key in self.info.keys():
@@ -240,9 +263,9 @@ class JAXDictList(MutableMapping):
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
-        obj = cls(aux_data['shape'], aux_data['info'])
+        obj = cls(aux_data["shape"], aux_data["info"])
         dict_data, additional_data = children
-        for (key, val) in zip(aux_data['info'].keys(), dict_data):
+        for key, val in zip(aux_data["info"].keys(), dict_data):
             obj.data[key] = val
         obj.additional_data = additional_data
         return obj
